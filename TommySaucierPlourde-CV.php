7104f4edaaece8ea9a76cd033fbc73bb87d620a2
ToOM <saucier-plourde.tommy@optrickmedia.com>
2018-01-18 14:30:14 -0500

    Menu Flexbox fonctionnel

---
+++ TommySaucierPlourde-CV.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<!-- Font Awesome -->
+	<script src="https://use.fontawesome.com/fc14a13db3.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tommy Saucier Plourde | Curriculum</title>
+    <link rel="icon" type="image/png" href="http://optrickmedia.com/blogue/img/favicon.png">
+    <link href="css/---bootstrap.css" rel="stylesheet">
+		<link href="css/optrick.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/resizeNav.css" />
+</head>
+<body>
+	<header>
+		<nav class="flex uppercase">
+			<div class="brand">
+				<div class="center">
+					<h1 id="logo">TOMMY <br />SAUCIER <br />PLOURDE</h1>
+				</div> <!-- center -->
+			</div> <!-- brand -->
+			<div class="categorie">
+				<a href="#about">About</a>
+				<a href="#work">Work experience</a>
+				<a href="#education">Education</a>
+				<a href="#contact">Contact</a>
+			</div> <!-- categorie -->
+		</nav>
+	</header><!-- /header -->
+
+<!-- ==================================================
+Introduction sur mon profil
+================================================== -->
+     <article id="about">
+     	 <a href="http://optrickmedia.com/SaucierPlourdeTommy_CV.pdf"class="bouton download" download >Dowload PDF</a>
+        <div class="content-wrap">
+            <img class="profile-img col-narrow" src="img/tommysaucierplourde.jpg" alt="Tommy Saucier Plourde" />
+            <div class="col-wide">
+                <h1>Tommy Saucier Plourde</h1>
+                <h2>Graphique Designer & Front-end</h2>
+                <p>Curieux de nature et passionné par le design, je suis constamment à l’affût des nouveautés et des tendances afin d’optimiser mes techniques de travail et garde ma créativité stimulée. Devant une problématique, j’oriente ma réflexion sur les besoins des utilisateurs avec trois objectifs en tête: intuitivité, performance et esthétisme.</p>
+            </div>
+        </div> <!-- /content-wrap -->
+    </article>
+	<main>
+<!-- Work -->
+    <section id="work" class="work">
+    	<div class="content-wrap">
+        	<h2>Expérience</h2><br />
+					<!-- 2e emploi -->
+					<div class="col-narrow">
+						<h3>Designer graphique & developeur web</h3>
+						<p class="uppercase">Optrick Media / Freelance</p>
+						<p>Septembre 2013 / Aujourd'hui</p>
+						<p>4 ans 5 mois</p>
+					</div>
+					<!-- Details -->
+					<div class="col-wide job-description">
+						<ul>
+							<li>PROJET AQUA INGENIUM</li>
+							<li>· Concevoir et développer un site Web réactif;</li>
+							<li>· Intégrer des animations sur le site Web afin de dynamiser le contenu;</li>
+							<li>· Produire des pièces imprimées pour un kiosque.</li>
+							<br />
+							<li>Voir le projet: <a href="http://aquaingenium.com/" target="_blank"><strong>AQUA INGENIUM</strong></a>.</li>
+
+							<br />
+							<li>PROJET TEAM BASKA</li>
+							<li>· Concevoir l’image de marque et les normes visuelles de l’entreprise;</li>
+							<li>· Produire des campagnes publicitaires pour des galas de boxes (médias sociaux, imprimé);</li>
+							<li>· Produire des objets promotionnels pour l’équipe et la marque.</li>
+							<br />
+							<li>Voir le projet: <a href="http://optrickmedia.com/portfolio-teambaska.html" target="_blank"><strong>TEAM BASKA</strong></a>.</li>
+						</ul>
+
+					</div>
+					<!-- 1er emploi -->
+          <div class="col-narrow">
+          	<h3>Designer graphique</h3>
+              <p class="uppercase">Mass-Média Capitale</p>
+              <p>Août 2016 / Août 2017</p>
+							<p>1 an</p>
+          </div>
+          <!-- Details -->
+          <div class="col-wide job-description">
+						<ul>
+							<li>PROJET VALCARTIER / CALYPSO</li>
+							<li>· Concevoir et préparer pour l’impression le territoire visuel de l’hôtel de Valcartier;</li>
+							<li>· Produire du matériel publicitaire selon les normes des marques (imprimé, web et médias sociaux);</li>
+							<li>· Produire l’affichage des parcs (signalétique, panneaux géants, menus);</li>
+							<li>· Préparer des maquettes et des montages vidéo servant à la publicité télévisée.</li>
+							<br />
+							<li>Voir le projet: <a href="http://aquaingenium.com/" target="_blank"><strong> ------</strong></a>.</li>
+
+							<br />
+							<li>PROJET VALCARTIER / CALYPSO</li>
+							<li>· Décliner les campagnes publicitaires pour les différents médias (imprimé, web, téléviseur);</li>
+							<li>· Assurer la qualité des livrables d’équipes de création externes;</li>
+							<li>· Participer au tournage de publicité.</li>
+							<br />
+							<li>Voir le projet: <a href="http://aquaingenium.com/" target="_blank"><strong> ------</strong></a>.</li>
+
+							<br />
+							<li>PROJET KIA QUÉBEC et KIA STE-FOY</li>
+							<li>· Décliner les campagnes publicitaires selon les normes de KIA  (imprimé, web, médias sociaux);</li>
+							<li>· Produire le matériel visuel et publicitaire interne des concessions.</li>
+							<br />
+							<li>Voir le projet: <a href="http://aquaingenium.com/" target="_blank"><strong> ------</strong></a>.</li>
+
+							<br />
+							<li>PROJET CONDO LB9</li>
+							<li>· Concevoir et développer un site Web réactif;</li>
+							<li>· Produire les campagnes publicitaires selon les normes de l’entreprise  (journal);</li>
+							<li>· Assurer le suivi des campagnes publicitaires avec le client.</li>
+							<br />
+							<li>Voir le projet: <a href="http://aquaingenium.com/" target="_blank"><strong> ------</strong></a>.</li>
+						</ul>
+					</div>
+      	</div> <!-- content-wrap -->
+  	</section> <!-- section-work -->
+  	<!-- Education -->
+    <section id="education" class="education">
+    	<div class="content-wrap">
+                <h2>Education</h2>
+                <h3>Université Laval - Québ<g xmlns="http://www.w3.org/2000/svg">
+<path fill="#FAFAFA" d="M129.643,170.581c-5.521,6.148-12.702,15.284-19.937,26.85c4.678,3.302,13.659,10.927,19.886,24.061    c12.44-7.229,24.074-13.047,30.662-18.663c43.051-36.677,148.469-168.15,144.844-171.954    C301.292,26.842,167.55,128.517,129.643,170.581z M107.911,198.512c-18.698-3.348-41.958,13.022-54.277,42.911    c-12.335,29.889-34.357,41.851-38.629,41.121c22.985,8.416,97.885,25.527,115.304-56.394    C122.908,206.502,107.911,198.512,107.911,198.512z"/>
+<path fill="#2B6EB6" d="M56.533,295c-0.003,0-0.004,0-0.008,0c-19.9-0.001-36.869-5.428-43.24-7.761    c-2.428-0.89-3.778-3.482-3.112-5.981c0.629-2.365,2.867-3.899,5.267-3.695c4.098-0.615,22.677-11.646,33.571-38.047    c11.448-27.772,33.141-46.434,53.979-46.434c1.154,0,2.303,0.06,3.437,0.177c6.095-9.57,12.976-18.758,19.495-26.019    c16.346-18.139,53.317-50.277,98.904-85.976c18.783-14.709,36.987-28.395,51.259-38.536    c23.884-16.971,26.636-16.971,28.646-16.971c1.555,0,2.977,0.599,4.002,1.685c1.007,1.057,1.526,2.5,1.446,4.043    c-0.096,1.848-0.241,4.641-17.892,28.048c-10.634,14.102-24.854,31.979-40.041,50.338c-37.109,44.86-70.288,81.033-88.751,96.763    c-5.197,4.43-12.858,8.724-21.729,13.695c-2.235,1.254-4.526,2.537-6.852,3.866c0.024,0.063,0.048,0.127,0.072,0.19    c0.337,0.895,0.41,1.867,0.211,2.803C123.283,283.232,85.844,295,56.533,295z M29.111,281.57    c7.389,1.799,16.967,3.429,27.416,3.43c0.002,0,0.005,0,0.007,0c36.802,0,59.864-19.658,68.567-58.437    c-5.875-14.389-16.242-21.591-18.917-23.264c-1.042-0.145-2.112-0.218-3.193-0.218c-16.572,0-34.967,16.55-44.735,40.246    C50.642,261.778,38.879,274.641,29.111,281.57z M116.383,196.274c4.509,3.776,10.335,9.741,15.23,18.295    c1.785-1.012,3.541-1.996,5.265-2.962c8.413-4.716,15.679-8.788,20.132-12.584c35.25-30.03,107.897-118.963,133.882-154.5    c-36.668,25.12-127.98,96.608-157.536,129.406C129.982,177.688,123.561,185.315,116.383,196.274z"/>
+</g>ec, CA</h3>
+                <p>BACCALAURÉAT Design Graphique</p>
+                <p>Mai 2015</p>
+                <h3>Cegep de Matane - Matane, CA</h3>
+                <p>Diplômes collégial - Intégration multimedia</p>
+                <p>Mai 2009</p>
+    	</div> <!-- content-wrap -->
+    </section>
+  </main>
+
+    <!-- Fotter & contact info -->
+    <footer id="contact">
+        <div class="content-wrap">
+            <h2>Contactez-moi!</h2>
+
+            <!-- Social media link. -->
+            <div class="contact-info">
+                <a href="mailto:saucier-plourde.tommy@optrickmedia.com"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i><span class="sr-only">Écrivez-moi</span></a>
+                <a href="https://www.linkedin.com/in/tommysaucierplourde/"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i><span class="sr-only">mon Linkedin</span></a>
+                <a href="http://optrickmedia.com/"><i class="fa fa-suitcase fa-2x" aria-hidden="true"></i><span class="sr-only">mon Portfolio</span></a>
+                <a href="https://www.behance.net/optrickmedia"><i class="fa fa-behance fa-2x" aria-hidden="true"></i><span class="sr-only">mon Behance</span></a>
+            </div>
+                        <!-- Social media link. -->
+            <div class="contact-info">
+				<p>Copyright 2017 par Tommy Saucier Plourde</p>
+            </div>
+
+        </div>  <!-- content-wrap -->
+    </footer>
+</body>
+</html>
